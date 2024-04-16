426c3025f27aeb1b4ecf4b070d5976b1f86b60cf
skoriop <karthikp.ivy@gmail.com>
2024-04-17 00:23:50 +0530

    fix: fix more psycopg2 bugs

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -6,12 +6,13 @@ def create_user(user: User):
     query = """
         INSERT INTO users (name, phone_number, email, from_bits, bits_id, affiliation, spent)
         VALUES (%(name)s, %(phone_number)s, %(email)s, %(from_bits)s, %(bits_id)s, %(affiliation)s, %(spent)s)
-        RETURNING id
+        RETURNING *
     """
     with PgDatabase() as db:
-        user_id = db.cursor.execute(query, vars(user))
+        db.cursor.execute(query, vars(user))
+        user_record = db.cursor.fetchone()
         db.connection.commit()
-        return user_id
+        return user_record
     
 # Gets a user by their ID
 def get_user_by_id(user_id: int):
@@ -30,11 +31,13 @@ def update_user(user_id: int, user: User):
         UPDATE users
         SET name = %(name)s, phone_number = %(phone_number)s, email = %(email)s, from_bits = %(from_bits)s, bits_id = %(bits_id)s, affiliation = %(affiliation)s, spent = %(spent)s
         WHERE id = %(user_id)s
+        RETURNING *
     """
     with PgDatabase() as db:
         db.cursor.execute(query, {'user_id': user_id, **vars(user)})
+        new_user = db.cursor.fetchone()
         db.connection.commit()
-        return user
+        return new_user
 
 # Delete a user
 def delete_user(user_id: int):
