347c38b21db7ba1165f1c2b8ad3a2326fe605bef
Aman Ranjan <ranjanaman0711@gmail.com>
2024-04-16 10:30:23 +0530

    feat(routers): update user rpute
    1. Refactored crud import so that route functions don't collide with crud functions
    2. Update route for '/user/:id/update'

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,13 +1,13 @@
 import app
-from app.crud.user import *
+import app.crud.user as user_crud
 from app.models.user import User
 
 # Create User route
 @app.post('/user/create')
 async def create_user(user: User):
     try:
-        user_id = create_user(user)
-        created_user = get_user_by_id(user_id)
+        user_id = user_crud.create_user(user)
+        created_user = user_crud.get_user_by_id(user_id)
         return {
             'error': False,
             'message': 'User created successfully',
@@ -24,11 +24,28 @@ async def create_user(user: User):
 @app.get('/user/{user_id}/')
 async def get_user(user_id: int):
     try:
-        user = get_user_by_id(user_id)
+        user = user_crud.get_user_by_id(user_id)
         return {
             'error': False,
             'message': None,
             'data': user
+        }
+    except Exception as e:
+        return {
+            'error': True,
+            'message': 'An error occurred: ' + str(e),
+            'data': None
+        }
+
+# Update User route
+@app.put('/user/{user_id}/update')
+async def update_user(user_id: int, user: User):
+    try:
+        updated_user = user_crud.update_user(user_id, user)
+        return {
+            'error': False,
+            'message': 'User updated successfully',
+            'data': updated_user
         }
     except Exception as e:
         return {
