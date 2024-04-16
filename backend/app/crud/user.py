f49d3c11b1ce8a61c24840b8d9875c6f700bf029
Aman Ranjan <ranjanaman0711@gmail.com>
2024-04-16 10:03:02 +0530

    feat(crud): update user function

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -22,3 +22,15 @@ def get_user_by_id(user_id: int):
         user = db.query_one(query, {'user_id': user_id})
         db.connection.commit()
         return user
+
+# Update a user profile
+def update_user(user_id: int, user: User):
+    query = """
+        UPDATE users
+        SET name = :name, phonenumber = :phonenumber, email = :email, from_bits = :from_bits, bits_id = :bits_id, affiliation = :affiliation, spent = :spent
+        WHERE id = :user_id
+    """
+    with PgDatabase() as db:
+        db.execute(query, {'user_id': user_id, **vars(user)})
+        db.connection.commit()
+        return user
