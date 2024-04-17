c00401fbdb539772fe45ddfe0e42df034fb01a8a
Aman Ranjan <ranjanaman0711@gmail.com>
2024-04-17 19:35:55 +0530

    feat(routers): update club route

--- backend/app/routers/clubs.py
+++ backend/app/routers/clubs.py
@@ -29,3 +29,14 @@ async def get_club(club_id: int):
         raise e
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    
+@router.post("/{club_id}/update")
+async def update_club(club_id: int, club: Club):
+    try:
+        updated_club = clubs_api.update_club_by_id(club_id, club)
+        return {
+            'message': 'Club updated successfully',
+            'data': updated_club
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
