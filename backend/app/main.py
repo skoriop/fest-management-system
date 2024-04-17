2a65e85f47729cbba0f52e08b03cfff28d9af915
skoriop <karthikp.ivy@gmail.com>
2024-04-17 17:49:00 +0530

    lint: format python with `black`

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-from .routers import user,item
+from .routers import user, item
 
 app = FastAPI()
+
 
 @app.exception_handler(StarletteHTTPException)
 async def http_exception_handler(request, exc):
     return JSONResponse(
         status_code=exc.status_code if exc.status_code is not None else 500,
-        content={'message': exc.detail, 'data': None}
+        content={"message": exc.detail, "data": None},
     )
 
-app.include_router(user.router, prefix='/user')
-app.include_router(item.router, prefix='/vendor')
-@app.get('/')
+
+app.include_router(user.router, prefix="/user")
+app.include_router(item.router, prefix="/vendor")
+
+
+@app.get("/")
 async def index():
-    return {'message': 'App'}
+    return {"message": "App"}
--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -4,50 +4,42 @@ from app.models.user import User
 
 router = APIRouter()
 
-@router.post('/create')
+
+@router.post("/create")
 async def create(user: User):
     try:
         created_user = user_api.create_user(user)
-        return {
-            'message': 'User created successfully',
-            'data': created_user
-        }
+        return {"message": "User created successfully", "data": created_user}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occurred: ${str(e)}')
-    
-@router.get('/{user_id}')
+        raise HTTPException(status_code=500, detail=f"An error occurred: ${str(e)}")
+
+
+@router.get("/{user_id}")
 async def get(user_id: int):
     try:
         user = user_api.get_user_by_id(user_id)
         if not user:
-            raise HTTPException(status_code=404, detail='User not found')
-        return {
-            'message': None,
-            'data': user
-        }
+            raise HTTPException(status_code=404, detail="User not found")
+        return {"message": None, "data": user}
     except HTTPException as e:
         raise e
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occurred: ${str(e)}')
+        raise HTTPException(status_code=500, detail=f"An error occurred: ${str(e)}")
 
-@router.post('/{user_id}/update')
+
+@router.post("/{user_id}/update")
 async def update(user_id: int, user: User):
     try:
         updated_user = user_api.update_user(user_id, user)
-        return {
-            'message': 'User updated successfully',
-            'data': updated_user
-        }
+        return {"message": "User updated successfully", "data": updated_user}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occurred: ${str(e)}')
-    
-@router.post('/{user_id}/delete')
+        raise HTTPException(status_code=500, detail=f"An error occurred: ${str(e)}")
+
+
+@router.post("/{user_id}/delete")
 async def delete_user(user_id: int):
     try:
         user_api.delete_user(user_id)
-        return {
-            'message': 'User deleted successfully',
-            'data': None
-        }
+        return {"message": "User deleted successfully", "data": None}
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f'An error occurred: ${str(e)}')
+        raise HTTPException(status_code=500, detail=f"An error occurred: ${str(e)}")
