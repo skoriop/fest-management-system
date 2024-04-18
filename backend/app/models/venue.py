3af1359987d2c2981af77ee82a087d3b3bf33f51
Aman Ranjan <ranjanaman0711@gmail.com>
2024-04-19 00:10:36 +0530

    feat(model): Venue, VenueEvent models

---
+++ backend/app/models/venue.py
@@ -0,0 +1,12 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class Venue(BaseModel):
+    id: Optional[int] = None
+    name: str
+    capacity: int
+    venue_type: str
+
+class VenueEvent(BaseModel):
+     venue_id: int
+     event_id: int
