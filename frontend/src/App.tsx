6c6cfb8aec5404a0eb91e05c05101e6a1c98bbd8
soumitradev <soumitra.shewale.dev@gmail.com>
2024-04-21 14:51:34 +0530

    feat: add all pages to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,6 +33,11 @@ import AddEvent from "./pages/AddEvent";
 import EventUpdate from "./pages/EventUpdate";
 import Events from "./pages/Events";
 import EventRegister from "./pages/EventRegister";
+import UserRegistrations from "./pages/UserRegistrations";
+import Venues from "./pages/Venues";
+import { VenueProvider } from "./contexts/VenueContext";
+import VenueCreate from "./pages/VenueCreate";
+import Venue from "./pages/Venue";
 import VendorSales from "./pages/VendorSales";
 
 function App() {
@@ -45,6 +50,100 @@ function App() {
 							<MemberProvider>
 								<OrderProvider>
 									<EventProvider>
+										<VenueProvider>
+											<Routes>
+												<Route element={<Home />} path="/" />
+												//Venues
+												<Route element={<Venues />} path="/venue" />
+												<Route element={<Venue />} path="/venue/:id" />
+												<Route element={<VenueCreate />} path="/venue/create" />
+												//Events
+												<Route element={<Events />} path="/event" />
+												<Route
+													element={<EventRegister />}
+													path="/event/:id/register"
+												/>
+												//Clubs
+												<Route element={<Clubs />} path="/club" />
+												<Route element={<ClubCreate />} path="/club/create" />
+												<Route element={<Club />} path="/club/:id" />
+												<Route
+													element={<ClubUpdate />}
+													path="/club/:id/update"
+												/>
+												<Route
+													element={<ClubMembers />}
+													path="/club/:id/members"
+												/>
+												<Route
+													element={<ClubEvents />}
+													path="/club/:id/events"
+												/>
+												<Route
+													element={<AddEvent />}
+													path="/club/:id/events/add"
+												/>
+												<Route
+													element={<EventUpdate />}
+													path="/club/:id/events/:eventid/update"
+												/>
+												<Route
+													element={<AddMember />}
+													path="/club/:id/members/add"
+												/>
+												//Vendors
+												<Route element={<Vendors />} path="/vendor" />
+												<Route
+													element={<VendorCreate />}
+													path="/vendor/create"
+												/>
+												<Route element={<Vendor />} path="/vendor/:id" />
+												<Route
+													element={<VendorUpdate />}
+													path="/vendor/:id/update"
+												/>
+												//Users
+												<Route element={<Users />} path="/user" />
+												<Route element={<User />} path="/user/:id" />
+												<Route element={<UserCreate />} path="/user/create" />
+												<Route
+													element={<UserUpdate />}
+													path="/user/:id/update"
+												/>
+												<Route
+													element={<VendorSales />}
+													path="/vendor/:id/sales"
+												/>
+												//Items
+												<Route element={<Items />} path="vendor/:id/item" />
+												<Route
+													element={<Item />}
+													path="vendor/:id/item/:itemid"
+												/>
+												<Route
+													element={<ItemCreate />}
+													path="vendor/:id/item/create"
+												/>
+												<Route
+													element={<ItemUpdate />}
+													path="vendor/:id/item/:itemid/update"
+												/>
+												//Orders
+												<Route
+													element={<OrderCreate />}
+													path="user/:id/order/create"
+												/>
+												<Route element={<UserOrders />} path="user/:id/order" />
+												<Route
+													element={<UserRegistrations />}
+													path="user/:id/registrations"
+												/>
+												<Route
+													element={<UserOrder />}
+													path="user/:id/order/:orderid"
+												/>
+											</Routes>
+										</VenueProvider>
 										<Routes>
 											<Route element={<Home />} path="/" />
 											//Events
